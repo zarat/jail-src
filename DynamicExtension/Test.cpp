8b03512b13e5f2f6189470f7c9f3a2aae57aae15
Manuel Zarat <manuel@zarat.ml>
2024-11-19 20:00:27 +0100

    Update Test.cpp

--- DynamicExtension/Test.cpp
+++ DynamicExtension/Test.cpp
@@ -2,19 +2,31 @@
 
 #include <string>
 
+#include <iostream> 
+
 using namespace JAIL;
 
 extern "C" {
 
-    __declspec(dllexport) void scTest(JAIL::JObject *c, void *) {
+    __declspec(dllexport) void scReadLine(JAIL::JObject *c, void *) {
 
-        printf("Hello moon");
+        std::string str; 
+        std::getline(std::cin, str);
+        c->getReturnVar()->setString(str.c_str());
+
+    }
+	
+	__declspec(dllexport) void scPrintLine(JAIL::JObject *c, void *) {
+
+        const char *str = c->getParameter("v")->getString().c_str();
+		printf("Test.printline(%s)", str);
 
     }
 
-    __declspec(dllexport) void registerTest(JAIL::JInterpreter *interpreter) {
+    __declspec(dllexport) void registerLib(JAIL::JInterpreter *interpreter) {
         
-        interpreter->addNative("function Test.test(v)", scTest, 0);
+        interpreter->addNative("function Test.readline()", scReadLine, 0);
+		interpreter->addNative("function Test.printline(v)", scPrintLine, 0);
 
     }
 
