5219907ffef6f217f15317729f2de3148f407b16
Manuel Zarat <manuel@zarat.ml>
2025-04-29 10:18:44 +0200

    Update _mk.sh

--- Static/_mk.sh
+++ Static/_mk.sh
@@ -1,1 +1,6 @@
-g++ main.cpp -o main -I. -L. -lJail
+# This script will create a binary "jail", statically compiled using "libJail.a"
+# apt install g++ dos2unix -y
+# dos2unix _mk.sh
+# sh _mk.sh
+
+g++ main.cpp -o jail -I. -L. -lJail
