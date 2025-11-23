82bd8a13483a6139469a06f58164cd2ae9ae201a
Manuel Zarat <manuel.zarat@gmail.com>
2025-11-23 23:53:43 +0100

    Update mk.sh

--- Lib/mk.sh
+++ Lib/mk.sh
@@ -3,6 +3,10 @@
 # dos2unix mk.sh
 # sh mk.sh
 
-g++ -c Jail.cpp -std=c++11 -I.
-ar crf libJail.a Jail.o
+src="Char.cpp Integer.cpp Double.cpp String.cpp Array.cpp Object.cpp Jail_Functions.cpp Jail_MathFunctions.cpp"
+obj="Char.o Integer.o Double.o String.o Array.o Object.o Jail_Functions.o Jail_MathFunctions.o"
+
+g++ -c Jail.cpp $src -std=c++11 -I.
+ar crf libJail.a Jail.o $obj
 cp libJail.a ../Static
+
