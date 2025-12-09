97bb643b2824309a2ea5740d50c3c96f60e4bf49
Manuel Zarat <manuel.zarat@gmail.com>
2025-12-09 20:17:56 +0100

    Modify mk.bat to use static linking options

--- DynamicExtension/mysql/mk.bat
+++ DynamicExtension/mysql/mk.bat
@@ -1,5 +1,5 @@
 g++ -c mysql_connector.cpp example.cpp -Iinclude -Llib -lmysql32
-g++ -shared mysql_connector.o example.o -o mysql.dll -Iinclude -Llib -L. -L.. -ljail -l:libmysql32.lib
+g++ -static-libgcc -static-libstdc++ -shared mysql_connector.o example.o -o mysql.dll -Iinclude -Llib -L. -L.. -ljail -l:libmysql32.lib
 
 @rem g++ mysql_connector.cpp example.cpp -o mysql -Iinclude -Llib -l:libmysql32.lib
 
