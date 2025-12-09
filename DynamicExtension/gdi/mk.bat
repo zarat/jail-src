f54387f42a6f5ee2da7efa4d569ada32f2c7575a
Manuel Zarat <manuel.zarat@gmail.com>
2025-12-09 20:01:04 +0100

    Update mk.bat

--- DynamicExtension/gdi/mk.bat
+++ DynamicExtension/gdi/mk.bat
@@ -1,4 +1,4 @@
 g++ -c main.cpp -I. -L. -lJail -lgdiplus -lole32 -std=c++14
-g++ -shared main.o -o GDI.dll -I. -L. -lJail -lgdiplus -lole32 -std=c++14
+g++ -static-libgcc -static-libstdc++ -shared main.o -o GDI.dll -I. -L. -lJail -lgdiplus -lole32 -std=c++14
 
 pause
