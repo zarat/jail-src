b317faa27ce19caf11efae1f4f4525a4ccbf22aa
Manuel Zarat <manuel.zarat@gmail.com>
2025-12-09 08:29:26 +0100

    Update mktest.bat

--- DynamicExtension/mktest.bat
+++ DynamicExtension/mktest.bat
@@ -1,8 +1,2 @@
 g++ -c Test.cpp
-g++ -shared Test.o -o Test.dll -L. -ljail
-
-g++ -c Jail_Functions.cpp
-g++ -shared Jail_Functions.o -o Std.dll -L. -ljail
-
-g++ -c Jail_MathFunctions.cpp
-g++ -shared Jail_MathFunctions.o -o Math.dll -L. -ljail
+g++ -static-libgcc -static-libstdc++ -shared Test.o -o Test.dll -L. -ljail
